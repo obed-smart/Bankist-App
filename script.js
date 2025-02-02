4ebbdb7eac474637dcb0b19931c4b68635c760f4
chukwu obed <codingwithsmart@gmail.com>
2025-02-02 19:22:33 +0000

    make it look responsive

--- script.js
+++ script.js
@@ -1,39 +1,64 @@
-"use strict";
+'use strict';
+
+const openTransaction = document.getElementById('open-transaction');
 
 // Data
 
 const account1 = {
-  owner: "Jonas Schmedtmann",
+  owner: 'Jonas Schmedtmann',
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-  interestRate: 1.2, 
+  interestRate: 1.2,
   pin: 1111,
-  type: "premium",
+  type: 'premium',
 };
 
 const account2 = {
-  owner: "Jessica Davis",
+  owner: 'Jessica Davis',
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
-  type: "standard",
+  type: 'standard',
 };
 
 const account3 = {
-  owner: "Steven Thomas Williams",
+  owner: 'Steven Thomas Williams',
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
-  type: "premium",
+  type: 'premium',
 };
 
 const account4 = {
-  owner: "Sarah Smith",
+  owner: 'Sarah Smith',
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
-  type: "basic",
+  type: 'basic',
 };
 
 const accounts = [account1, account2, account3, account4];
 
-console.log(accounts)
+console.log(accounts);
+
+// add event to open and close transactions log
+// change the icon from up to down or vice vacer
+
+openTransaction.addEventListener('click', e => {
+  // Get the ion-icon element inside the button
+  const icon = e.target;
+
+  // Get the transaction container
+  // add open the scale to full
+  const transaction = document.querySelector('.MOVEMENTS');
+  transaction.style.display = "block"
+  transaction.classList.toggle('animate');
+  
+
+  // change the icon by name
+  icon.setAttribute(
+    'name',
+    icon.getAttribute('name') === 'chevron-up-outline'
+      ? 'chevron-down-outline'
+      : 'chevron-up-outline'
+  );
+});
