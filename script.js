8404a9cc68956f0597bb79b53b826d08dc95c4af
chukwu obed <codingwithsmart@gmail.com>
2025-02-03 17:48:04 +0000

    added login feature and colapsing of the transaction logs

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 'use strict';
 
 const openTransaction = document.getElementById('open-transaction');
+const logINForm = document.querySelectorAll('.login-form');
 
 // Data
-
 const account1 = {
   owner: 'Jonas Schmedtmann',
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
@@ -50,15 +50,84 @@ openTransaction.addEventListener('click', e => {
   // Get the transaction container
   // add open the scale to full
   const transaction = document.querySelector('.MOVEMENTS');
-  transaction.style.display = "block"
-  transaction.classList.toggle('animate');
-  
+  transaction.style.display = 'block';
+
+  if (transaction.classList.contains('animateUP')) {
+    transaction.classList.remove('animateUP');
+    transaction.classList.add('animateDown');
+  } else {
+    transaction.classList.add('animateUP');
+    transaction.classList.remove('animateDown');
+  }
 
   // change the icon by name
   icon.setAttribute(
     'name',
-    icon.getAttribute('name') === 'chevron-up-outline'
-      ? 'chevron-down-outline'
-      : 'chevron-up-outline'
+    icon.getAttribute('name') === 'chevron-down-outline'
+      ? 'chevron-up-outline'
+      : 'chevron-down-outline'
   );
 });
+
+// submit the user login details
+
+logINForm.forEach(form => {
+  form.addEventListener('submit', logIN);
+});
+
+/**
+ * Display the transaction logs of the user.
+ * @param {object} data - The details and transaction history of the user
+ */
+function displayTransactionLogs(data) {
+  const container = document.getElementById('logs');
+  const fragment = document.createDocumentFragment();
+  for (const log of data) {
+    const item = ` <div
+              class="flex px-8 max-md:px-4 max-md:text-[14px] py-6 items-center justify-between border-b-[2px] border-b-[#eee]"
+            >
+              <div
+                class="uppercase  ${
+                  log > 0
+                    ? 'bg-linear-[to_top_left,#39b385,#9be15d]'
+                    : 'bg-linear-[to_top_left,#e52a5a,#ff585f]'
+                } px-4 py-1 text-[#fff] rounded-full font-medium"
+              >
+                ${log > 0 ? '2 deposit' : '1 withdrawal'}
+              </div>
+              <div class="text-[#666]">3 days ago</div>
+              <div class=" font-medium ${
+                log > 0 ? 'text-green-500' : 'text-red-500'
+              } ">${log}€</div>
+            </div>`;
+    container.insertAdjacentHTML("afterbegin" , item)
+  }
+}
+
+/**
+ * login the user if the accout matches
+ * @param {form} e - get the form to be submited
+ */
+function logIN(e) {
+  e.preventDefault(); // prevent the default form submission
+
+  const userName = e.target.querySelector('.user-name'); // get the targeted form username
+  const userKey = e.target.querySelector('.user-password'); // get the targeted form password
+  const wellcomeText = document.getElementById('logo-text');
+
+  // find account that matches the userName
+  const userAccount = accounts.find(user => user.owner === userName.value);
+  if (!userAccount) return;
+
+  if (userAccount) {
+    //confirm the password
+    if (userAccount.pin === Number(userKey.value)) {
+      displayTransactionLogs(userAccount.movements);
+      wellcomeText.textContent = `wellcome ${userAccount.owner.split(' ')[0]}`;
+      document.body.classList.add('islogin');
+    } else {
+      userKey.classList.add('border-[1px]');
+      userKey.classList.add('border-red-400');
+    }
+  }
+}
