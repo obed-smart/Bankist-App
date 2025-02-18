46d1c12f7029824cc974cd890337c728a1e3f91a
chukwu obed <codingwithsmart@gmail.com>
2025-02-18 02:16:54 +0000

    implemented international date , time , and current formater based in the user locale or the user.current choose

--- script.js
+++ script.js
@@ -7,12 +7,25 @@ const logOutButton = document.getElementById('logout-btn');
 const transferForm = document.querySelector('.transfer');
 const closeAccountForm = document.querySelector('.close-account');
 const loanMoneyForm = document.querySelector('.loan-money');
+const sortBtn = document.querySelector('.sort-btn');
+const currentTime = document.querySelectorAll('.current-time');
 
 // Data
 const account1 = {
   owner: 'js',
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2,
+  movementsDates: [
+    '2024-12-23T07:42:02.383Z',
+    '2024-12-30T09:15:04.904Z',
+    '2025-01-10T21:31:17.178Z',
+    '2025-02-01T00:00:00.000Z',
+    '2025-02-01T00:00:00.000Z',
+    '2025-02-14T00:00:00.000Z',
+    '2025-02-16T00:00:00.000Z',
+    '2025-02-18T00:00:00.000Z',
+  ],
+  currency: 'USD',
   pin: 1111,
   type: 'premium',
 };
@@ -21,6 +34,17 @@ const account2 = {
   owner: 'jd',
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
+  movementsDates: [
+    '2024-12-23T07:42:02.383Z',
+    '2024-12-30T09:15:04.904Z',
+    '2025-01-10T21:31:17.178Z',
+    '2025-02-01T00:00:00.000Z',
+    '2025-02-01T00:00:00.000Z',
+    '2025-02-14T00:00:00.000Z',
+    '2025-02-16T00:00:00.000Z',
+    '2025-02-18T00:00:00.000Z',
+  ],
+  currency: 'NGN',
   pin: 2222,
   type: 'standard',
 };
@@ -29,6 +53,17 @@ const account3 = {
   owner: 'Steven Thomas Williams',
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
+  movementsDates: [
+    '2024-12-23T07:42:02.383Z',
+    '2024-12-30T09:15:04.904Z',
+    '2025-01-10T21:31:17.178Z',
+    '2025-02-01T00:00:00.000Z',
+    '2025-02-01T00:00:00.000Z',
+    '2025-02-14T00:00:00.000Z',
+    '2025-02-16T00:00:00.000Z',
+    '2025-02-18T00:00:00.000Z',
+  ],
+  currency: 'GBP',
   pin: 3333,
   type: 'premium',
 };
@@ -37,6 +72,17 @@ const account4 = {
   owner: 'Sarah Smith',
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
+  movementsDates: [
+    '2024-12-23T07:42:02.383Z',
+    '2024-12-30T09:15:04.904Z',
+    '2025-01-10T21:31:17.178Z',
+    '2025-02-01T00:00:00.000Z',
+    '2025-02-01T00:00:00.000Z',
+    '2025-02-14T00:00:00.000Z',
+    '2025-02-16T00:00:00.000Z',
+    '2025-02-18T00:00:00.000Z',
+  ],
+  currency: 'EUR',
   pin: 4444,
   type: 'basic',
 };
@@ -141,16 +187,64 @@ function updateUI(currentUser) {
 }
 
 /**
+ * function to update and display date of transactions
+ * @param {date} date - date of the transaction
+ * @returns {date} The formated based on the user's locale
+ */
+function formatedTransactionDate(date) {
+  // get the different between the future and now
+  const calcPassedDate = (now, future) =>
+    Math.round(Math.abs(future - now) / (1000 * 60 * 60 * 24));
+
+  const passedDate = calcPassedDate(new Date(), date); // future date instance
+
+  if (passedDate === 0) return 'Today';
+  if (passedDate === 1) return 'Yesterday';
+  if (passedDate <= 7) return `${passedDate} days ago`;
+
+  // const day = `${date.getDate()}`.padStart(2, '0');
+  // const month = `${date.getMonth() + 1}`.padStart(2, '0');
+  // const year = date.getFullYear();
+
+  return new Intl.DateTimeFormat(navigator.language).format(date);
+}
+
+/**
+ * format the transactions based on user's locale
+ * @param {object} account  - The user account
+ * @returns {number} the formated money based on the currency and user's local
+ */
+function formatUserFunds(log, currency) {
+  const userMoneyOptions = {
+    style: 'currency',
+    currency: currency,
+    currencyDisplay: 'symbol',
+  };
+
+  // dynamically format user money
+  return new Intl.NumberFormat(navigator.language, userMoneyOptions).format(
+    log
+  );
+}
+
+/**
  * Display the transaction logs of the user.
  * @param {object} account - The details of the user
  */
-function displayTransactionLogs(account) {
+function displayTransactionLogs(account, sort = false) {
   const container = document.getElementById('logs');
   container.innerHTML = '';
   const { movements } = account;
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
 
-  const fragment = document.createDocumentFragment();
-  for (const [i, log] of movements.entries()) {
+  for (const [i, log] of movs.entries()) {
+    const date = new Date(account.movementsDates[i]); // get date from date array
+
+    const displayTime = formatedTransactionDate(date); // date to be deplayed on the page
+
+    // dynamically format user money
+    const userMoney = formatUserFunds(log, account.currency);
+
     const item = ` <div
               class="flex px-8 max-md:px-4 max-md:text-[14px] py-6 items-center justify-between border-b-[2px] border-b-[#eee]"
             >
@@ -163,10 +257,10 @@ function displayTransactionLogs(account) {
               >
                 ${log > 0 ? `${i + 1} deposit` : `${i + 1} withdrawal`}
               </div>
-              <div class="text-[#666]">3 days ago</div>
+              <div class="text-[#666]">${displayTime}</div>
               <div class=" font-medium ${
                 log > 0 ? 'text-green-500' : 'text-red-500'
-              } ">${log}€</div>
+              } ">${userMoney}</div>
             </div>`;
     container.insertAdjacentHTML('afterbegin', item);
   }
@@ -174,12 +268,36 @@ function displayTransactionLogs(account) {
   calcDisplaybalance(account);
   calcDisplaySummary(account);
 }
+//
+/**
+ * dispaly and update the current user account balance
+ * display and update the current user account time and date of checking
+ * @param {object} account - the user account
+ */
 
 function calcDisplaybalance(account) {
+  // formate current balance date
+  const now = new Date();
+  const localeTime = navigator.language;
+
+  const timeOptions = {
+    dateStyle: 'medium',
+    timeStyle: 'short',
+  };
+
+  for (const time of currentTime) {
+    time.textContent = new Intl.DateTimeFormat(localeTime, timeOptions).format(
+      now
+    );
+  }
+  // display current balance
   const { movements } = account;
   const balace = document.querySelector('.balance');
   account.balace = movements.reduce((arr, cur) => arr + cur, 0);
-  balace.textContent = `${account.balace}$`;
+
+  const userMoney = formatUserFunds(account.balace, account.currency);
+
+  balace.textContent = userMoney;
 }
 
 /**
@@ -189,12 +307,10 @@ function calcDisplaybalance(account) {
 function calcDisplaySummary(account) {
   let { movements, interestRate } = account; // get the transaction and interestRate of the user account
   const originalMove = [...movements]; // create another copy of the transaction login
-  let isSorted = false;
 
   const moneyIn = document.querySelector('.fund-in');
   const moneyOut = document.querySelector('.fund-out');
   const interest = document.querySelector('.fund-interest');
-  const sortBtn = document.querySelector('.sort-btn');
 
   // deposited money
   const deposit = movements
@@ -204,7 +320,7 @@ function calcDisplaySummary(account) {
   // withdrawed money
   const withdrawal = movements
     .filter(num => num < 0, 0)
-    .reduce((total, num) => total + num, 0);
+    .reduce((total, num) => Math.abs(total + num), 0);
 
   //interest rate
   const interestRates = movements
@@ -212,21 +328,9 @@ function calcDisplaySummary(account) {
     .map(deposit => deposit * (interestRate / 100))
     .reduce((total, int) => total + int, 0);
 
-  moneyIn.textContent = `${deposit}€`;
-  moneyOut.textContent = `${Math.abs(withdrawal)}€`;
-  interest.textContent = `${interestRates.toFixed(2)}€`;
-
-  // sort the transaction log
-  sortBtn.addEventListener('click', () => {
-    if (!isSorted) {
-      movements.sort();
-    } else {
-      movements = [...originalMove];
-    }
-
-    isSorted = !isSorted;
-    displayTransactionLogs(account);
-  });
+  moneyIn.textContent = formatUserFunds(deposit, account.currency);
+  moneyOut.textContent = formatUserFunds(withdrawal, account.currency);
+  interest.textContent = formatUserFunds(interestRates, account.currency);
 }
 
 /**
@@ -251,6 +355,9 @@ function transferMoney(e) {
   ) {
     currentUserAccount.movements.push(-amount);
     reciversAcc.movements.push(amount);
+
+    currentUserAccount.movementsDates.push(new Date().toISOString()); // add transfer date
+    reciversAcc.movementsDates.push(new Date().toISOString()); // add reciever date
     updateUI(currentUserAccount);
   }
 }
@@ -263,8 +370,15 @@ function loanMoney(e) {
   e.preventDefault();
   const inputAmout = e.target.querySelector('.load-amount');
   const amount = Number(inputAmout.value);
-  currentUserAccount?.movements.push(amount);
-  updateUI(currentUserAccount);
+
+  if (
+    amount > 0 &&
+    currentUserAccount.movements.some(mov => mov >= amount * 0.1)
+  ) {
+    currentUserAccount?.movements.push(amount);
+    currentUserAccount.movementsDates.push(new Date().toISOString()); // add load date
+    updateUI(currentUserAccount);
+  }
   inputAmout.value = '';
 }
 
@@ -295,6 +409,14 @@ function closeAccount(e) {
   }
   userName.value = userPin.value = '';
 }
+
+let sorted = false; // initializing a sort
+
+// sort the transaction log
+sortBtn.addEventListener('click', () => {
+  displayTransactionLogs(currentUserAccount, !sorted);
+  sorted = !sorted; // switching the sorted
+});
 
 // event clicks
 
